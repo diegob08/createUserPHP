11fd1b65225d4f9b2c9fcd6c5578141062635592
diegob08 <d_bernalgomez@fanshaweonline.ca>
2018-02-19 18:07:44 -0500

    user mail

--- admin/phpscripts/user.php
+++ admin/phpscripts/user.php
@@ -8,10 +8,24 @@ function createUser($fname, $username, $password, $email, $userlvl){
     if($userQuery){
       redirect_to("admin_index.php");
     }else{
-  $message = "There was a problem setting up this user. Maybe reconsider your hiring practices";
+  $message = "There was a problem setting up this user.";
 
   return $message;
 }
   mysqli_close($link);
+}
+
+function sendEmail($email, $username, $password)
+{
+
+    $to = $email;
+    $subject = "Welcome to the best movies platform";
+    $body = "Username: " . $username . "\r\n";
+    $body .= "Password: " . $password . "\r\n";
+    $body .= "Login URL: http://localhost/admin/admin_createuser.php"; //would work with any email configured 
+    $headers = 'From: noreply@test.com' . "\r\n";
+
+    echo $body;
+    mail($to, $subject, $body, $headers);
 }
 ?>
